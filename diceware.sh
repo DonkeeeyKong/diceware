58034e64f75c3378310f0f45c94d1d08fc087e04
DonkeeeyKong <135337455+DonkeeeyKong@users.noreply.github.com>
2024-12-14 20:17:57 +0100

    Add script

---
+++ diceware.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# 
+source "$(dirname "${0}")/diceware.conf"
+if [ -n "$1" ]
+then
+	wortanzahl="$1"
+else
+	wortanzahl="${defaultlength:=6}"
+fi
+dicewaredefault="$(dirname "${0}")/dicewaredefault.txt"
+if [ -f "$2" ]
+then
+	dicewaretemp=$(mktemp)
+	cat "$2" > "$dicewaretemp"
+	sed -i 's/\r$//' "$dicewaretemp"
+	dicewaredatei="$dicewaretemp"
+else
+	if [[ "$defaultdwfile" =~ ^https?://*|^ftp://*|^file://* ]]
+	then
+		if [[ "$(curl -sIkL "$defaultdwfile" | sed -r '/content-type:/I!d;s/.*content-type: (.*)$/\1/I')" =~ ^text/plain* ]] &&
+			[[ ! "$(curl --head --silent --output /dev/null --write-out '%{http_code}' "$defaultdwfile")" =~ ^404*|^403* ]]
+		then
+			if [[ $(curl -sIkL "$defaultdwfile" | sed -r '/filename=/!d;s/.*filename=(.*)$/\1/') ]]
+			then 
+				defaultdwfilename="$(dirname "${0}")/$(curl -skIL "$defaultdwfile" | sed -r '/filename=/!d;s/.*filename=(.*)$/\1/' | sed 's/\r//g' | sed 's/"//g')"
+			else
+				defaultdwfilename="$(dirname "${0}")/$(basename "$(curl "$defaultdwfile" -s -L -I -o /dev/null -w '%{url_effective}' | sed -e s/?viasf=1//)")"
+			fi
+			if [[ "$defaultdwfilename" = "$(basename "$(readlink "$dicewaredefault")")" ]]
+			then
+				curl --no-progress-meter -L -o "$defaultdwfilename" -z "$defaultdwfilename" "$defaultdwfile"
+			else
+				curl --no-progress-meter -L -o "$defaultdwfilename" "$defaultdwfile"
+			fi
+			ln -rfs "$defaultdwfilename" "$dicewaredefault"
+		elif [ -f "$(readlink "$dicewaredefault")" ]
+		then
+			echo -e "${defaultdwfile} is not a reachable plain text file.\nProceeding with '${dicewaredefault}'."
+		else
+			echo -e "${defaultdwfile} is not a reachable plain text file.\n'${dicewaredefault}' doesn't exist or doesn't point to a file.\nAborting."
+			exit 1
+		fi
+	else
+		ln -rfs "$defaultdwfile" "$dicewaredefault"
+	fi
+	sed -i 's/\r$//' "$(readlink "$dicewaredefault")"
+	dicewaredatei="$dicewaredefault"
+fi
+if grep -F "11111" "$dicewaredatei" >> /dev/null
+then
+	dwfilelength="$(awk '/11111/ {b=NR; next} /66666/ {print NR-b+1; exit}' "$dicewaredatei")"
+elif grep -F "1111" "$dicewaredatei" >> /dev/null
+then
+	dwfilelength="$(awk '/1111/ {b=NR; next} /6666/ {print NR-b+1; exit}' "$dicewaredatei")"
+else
+	echo "'${dicewaredatei}' does not contain correctly formatted numbers. Aborting."
+	exit 1
+fi
+declare -i i=0
+while [ $i -lt $wortanzahl ]
+do
+	if [[ "$dwfilelength" = "1296" ]]
+	then
+		woerter[i]="$(echo $(($RANDOM % 6 + 1))$(($RANDOM % 6 + 1))$(($RANDOM % 6 + 1))$(($RANDOM % 6 + 1)) | grep -Ff -  "$dicewaredatei" | awk '{print $2}')"
+	elif [[ "$dwfilelength" = "7776" ]]
+	then
+		woerter[i]="$(echo $(($RANDOM % 6 + 1))$(($RANDOM % 6 + 1))$(($RANDOM % 6 + 1))$(($RANDOM % 6 + 1))$(($RANDOM % 6 + 1)) | grep -Ff -  "$dicewaredatei" | awk '{print $2}')"
+	else
+		echo "A diceware list has to contain 1296 or 7776 words. '${dicewaredatei}' contains ${dwfilelength:=0} words. Aborting."
+		exit 1
+	fi
+	i=i+1
+done
+echo "${woerter[*]}"
+(IFS=$'-'; echo "${woerter[*]}")
+rm -f "$dicewaretemp"
+exit 0
